ce0d4523fe2ea5b8433972086cf8fd0a2f26122b
Aditya Singh <adityasingh3781@outlook.com>
2024-08-23 23:50:03 +0530

    Added codeforces Problem Primary Tasks

--- Arrays/src/primaryTask.java
+++ Arrays/src/primaryTask.java
@@ -4,23 +4,10 @@ public class primaryTask {
        Scanner in= new Scanner(System.in);
        int t=in.nextInt();
        while(t-->0){
-        int n=in.nextInt();
-        String str = Integer.toString(n);
-        String str2=str.substring(0, 2);
-        String str3=str.substring(2, 3);
-        if("10".equals(str2)){
-            if(Integer.parseInt(str3)==0){
-                System.out.println("NO");
-            }
-            else{
-                if(Integer.parseInt(str.substring(2, str.length()))>=2){
-                    System.out.println("YES");
-                }
-                else{
-                    System.out.println("NO");
-                }
+        String str=in.next();
+        if("10".equals(str.substring(0,2))&&str.length()>=3&&str.charAt(2)-'0'!=0 &&Integer.valueOf(str.substring(2))>=2){
+            System.out.println("YES");
         }
-    }
         else{
             System.out.println("NO");
         }
@@ -28,5 +15,4 @@ public class primaryTask {
         
     }
 }
-
 
