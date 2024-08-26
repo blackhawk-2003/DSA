c17af8fda20e677443e0fd58b36bd0ef174c9cdd
Aditya Singh <adityasingh3781@outlook.com>
2024-08-26 18:37:58 +0530

    Added codeforces All Distinct And Turtle and Good Strings

---
+++ Codeforces/src/AllDistinct.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class AllDistinct {
+
+    public static int calculateDistinct(int arr[]){
+        HashSet< Integer > set = new HashSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            set.add(arr[i]);
+        }
+        int k = set.size();
+        int j = 0;
+        for (int x: set) {
+            arr[j++] = x;
+        }
+        return k;
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int n = in.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = in.nextInt();
+            }
+           System.out.println(calculateDistinct(a));
+        }
+    }
+}
+
---
+++ Codeforces/src/TurtleAndGoodStrings.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class TurtleAndGoodStrings {
+     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while(t-->0){
+            int n = in.nextInt();
+            String s = in.next();
+            char[]str1 = s.toCharArray();
+            if(str1[0]==str1[n-1]){
+                System.out.println("NO");
+            }
+            else{
+                System.out.println("NO");
+            }
+        }
+     }
+}
