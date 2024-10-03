ea8c12ec27ba08c891fac2668705d4eee2b30c6e
Aditya Singh <adityasingh3781@outlook.com>
2024-10-03 22:55:31 +0530

    Made some changes in selection sort

--- Basic Sorting Algorithms/src/Selection_Sort.java
+++ Basic Sorting Algorithms/src/Selection_Sort.java
@@ -4,16 +4,16 @@ public class Selection_Sort {
 
     public static int[] Selection_Sort(int arr[]) {
         int n = arr.length;
-        for (int i = 0; i <= n - 1; i++) {
+        for (int i = 0; i < n - 1; i++) {
             int min = i;
-            for (int j = i + 1; j <= n - 1; j++) {
-                if (arr[i] > arr[j]) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[min] > arr[j]) {
                     min = j;
                 }
             }
-            int temp = arr[i];
-            arr[i] = arr[min];
-            arr[min] = temp;
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
         }
         return arr;
     }
